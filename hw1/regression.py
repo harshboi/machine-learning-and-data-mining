5f80781c317614fc242645c65f56d1d8f7b96a5b
Jordan Crane <jordancrane@me.com>
2017-04-07 12:18:08 -0700

    Generalize data import functions, add TODOs

--- hw1/regression.py
+++ hw1/regression.py
@@ -7,23 +7,23 @@ import numpy as np
 from numpy.linalg import inv
 
 ####### Part1 ########
-def build_training_data_arrays(training_file):
-    (features, outputs) = build_training_data_lists(training_file)
+def build_data_arrays(file):
+    (features, outputs) = build_data_lists(file)
     # Arrays are equivalent to matrices and vectors in Python
     return np.array(features, dtype=float), np.array(outputs, dtype=float)
 
-def build_training_data_lists(training_file):
+def build_data_lists(file):
     features = []
     outputs = []
-    for line in training_file:
+    for line in file:
         (line_features, line_output) = extract_features_and_output(line)
         features.append(line_features)
         outputs.append(line_output)
     return features, outputs
 
 def extract_features_and_output(line):
-    split_line = line.split()
-    return split_line[0:-1], split_line[-1]
+    features_and_output = line.split()
+    return features_and_output[0:-1], features_and_output[-1]
 
 def compute_weight_vector(X, y):
     # Formula for weight vector
@@ -31,12 +31,18 @@ def compute_weight_vector(X, y):
     return w
 
 def train_model():
-    training_file = open("housing_train.txt", 'r')
-    (features, outputs) = build_training_data_arrays(training_file)
+    training_file = open("data/housing_train.txt", 'r')
+    (features, outputs) = build_data_arrays(training_file)
     weight_vector = compute_weight_vector(features, outputs)
+    #TODO calculate SSE
     return weight_vector
+
+def test_model(weight_vector):
+    testing_file = open("data/housing_test.txt", "r")
+    (features, outputs) = build_data_arrays(testing_file)
+    #TODO run data through model
+    #TODO calculate SSE
 
 weight_vector = train_model()
 print weight_vector
-testing_file = open("housing_test.txt", "r")
-
+test_model(weight_vector)
