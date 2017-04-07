5224085761a04fc2728eaeee36fab80fc2fac736
brahmstadtn <brahmstadtn@gmail.com>
2017-04-07 12:37:49 -0700

    Merging

--- hw1/regression.py
+++ hw1/regression.py
@@ -7,26 +7,29 @@ import numpy as np
 from numpy.linalg import inv
 
 ####### Part1 ########
-def build_training_data_arrays(training_file):
-    (features, outputs) = build_training_data_lists(training_file)
+def build_data_arrays(file):
+    (features, outputs) = build_data_lists(file)
     # Arrays are equivalent to matrices and vectors in Python
     return np.array(features, dtype=float), np.array(outputs, dtype=float)
 
-def build_training_data_lists(training_file):
+def build_data_lists(file):
     features = []
     outputs = []
-    for line in training_file:
+    for line in file:
         (line_features, line_output) = extract_features_and_output(line)
         features.append(line_features)
         outputs.append(line_output)
     return features, outputs
 
 def extract_features_and_output(line):
-    split_line = line.split()
-    #Instert the dummy variable at the beginning of the array so that we can get a weight for the constant later
-    split_line.insert(0, 1)
+
+    features_and_output = line.split()
     
-    return split_line[0:-1], split_line[-1]
+    #Instert the dummy variable at the beginning of the array so that we can get a weight for the constant later
+    features_and_outputs.insert(0,1)
+    
+    return features_and_output[0:-1], features_and_output[-1]
+
 
 def compute_weight_vector(X, y):
     # Formula for weight vector
@@ -35,18 +38,25 @@ def compute_weight_vector(X, y):
 
     
 def train_model():
-    training_file = open("housing_train.txt", 'r')
-    (features, outputs) = build_training_data_arrays(training_file)
+    training_file = open("data/housing_train.txt", 'r')
+    (features, outputs) = build_data_arrays(training_file)
     weight_vector = compute_weight_vector(features, outputs)
+    #TODO calculate SSE
     return weight_vector
+
+
+def test_model(weight_vector):
+    testing_file = open("data/housing_test.txt", "r")
+    (features, outputs) = build_data_arrays(testing_file)
+    #TODO run data through model
+    #TODO calculate SSE
 
 #Don't print in scientific notation :)
 np.set_printoptions(suppress=True)
-
 weight_vector = train_model()
 
 print "Weight Vector:"
 print weight_vector
 
-testing_file = open("housing_test.txt", "r")
+test_model(weight_vector)
 
