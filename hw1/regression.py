89ba36e787b688ca02b61a633309973128a703d2
brahmstadtn <brahmstadtn@gmail.com>
2017-04-07 12:30:59 -0700

    Computing Weight vector with constant included

--- hw1/regression.py
+++ hw1/regression.py
@@ -23,20 +23,30 @@ def build_training_data_lists(training_file):
 
 def extract_features_and_output(line):
     split_line = line.split()
+    #Instert the dummy variable at the beginning of the array so that we can get a weight for the constant later
+    split_line.insert(0, 1)
+    
     return split_line[0:-1], split_line[-1]
 
 def compute_weight_vector(X, y):
     # Formula for weight vector
-    w = inv(X.T.dot(X)).dot(X.T).dot(y)
-    return w
+	w = inv(X.T.dot(X)).dot(X.T).dot(y)
+	return w
 
+    
 def train_model():
     training_file = open("housing_train.txt", 'r')
     (features, outputs) = build_training_data_arrays(training_file)
     weight_vector = compute_weight_vector(features, outputs)
     return weight_vector
 
+#Don't print in scientific notation :)
+np.set_printoptions(suppress=True)
+
 weight_vector = train_model()
+
+print "Weight Vector:"
 print weight_vector
+
 testing_file = open("housing_test.txt", "r")
 
