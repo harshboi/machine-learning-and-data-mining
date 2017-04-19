7e16330684c5d30f9f7cbe0fb3999298babd9eb2
brahmstadtn <brahmstadtn@gmail.com>
2017-04-19 13:52:31 -0700

    Really cool function added to show the weights as an image.

--- assignment-2/handwriting.py
+++ assignment-2/handwriting.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python2
 ################################################
-# CS434 Machine Learning and Data Mining	   #
-# Assignment 2								   #
-# Nathan Brahmstadt and Jordan Crane		   #
+# CS434 Machine Learning and Data Mining       #
+# Assignment 2                                 #
+# Nathan Brahmstadt and Jordan Crane           #
 ################################################
 import math
 import numpy as np
 from numpy.linalg import inv
 from numpy.linalg import norm
 from collections import namedtuple
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 ########## Data Structures ##########
 
@@ -19,115 +21,129 @@ labels = Labels(0, 1)
 ########## Main ##########
 
 def main():
-	(training_features, training_outputs) = get_data_arrays(
-			"data/usps-4-9-train.csv")
-	(testing_features, testing_outputs) = get_data_arrays(
-			"data/usps-4-9-test.csv")
-	
-	print "***** Problem 1 *****"
-	for i in range(5):
-	
-		learning_rate = float(10)/(10**i)
-		print "Learning Rate of: " + str(learning_rate)
-		weight = batch_gradient_descent(training_features, training_outputs, learning_rate, False)
-	   
-		train_acc = test_weight(training_features, training_outputs, weight)
-		
-		print "Training Data Accuracy: " + str(train_acc*100) + "%"
-		
-		test_acc = test_weight(testing_features, testing_outputs, weight)
-		
-		print "Testing Data Accuracy: " + str(test_acc*100) + "%"
-		
-	print "***** Problem 2 *****"
-	
-	weight = batch_gradient_descent(training_features, training_outputs, learning_rate, True)
-	
+    (training_features, training_outputs) = get_data_arrays(
+            "data/usps-4-9-train.csv")
+    (testing_features, testing_outputs) = get_data_arrays(
+            "data/usps-4-9-test.csv")
+    
+    print "***** Problem 1 *****"
+    for i in range(5):
+    
+        learning_rate = float(10)/(10**i)
+        print "Learning Rate of: " + str(learning_rate)
+        weight = batch_gradient_descent(training_features, training_outputs, learning_rate, False)
+       
+        train_acc = test_weight(training_features, training_outputs, weight)
+        
+        print "Training Data Accuracy: " + str(train_acc*100) + "%"
+        
+        test_acc = test_weight(testing_features, testing_outputs, weight)
+        
+        print "Testing Data Accuracy: " + str(test_acc*100) + "%"
+        
+    print "***** Problem 2 *****"
+    print "....Generating Accuracy Data..."
+    weight = batch_gradient_descent(training_features, training_outputs, 0.01, True)
+    print "Done"
+
+    print "***** Problem 3 *****"
+    
+    
   
-	
-	
+    
+    
 
 ########## Data Import ##########
 
 def get_data_arrays(filename):
-	file = open(filename)
-	(features, outputs) = build_data_arrays(file)
-	file.close()
-	return (features, outputs)
+    file = open(filename)
+    (features, outputs) = build_data_arrays(file)
+    file.close()
+    return (features, outputs)
 
 def build_data_arrays(file):
-	(features, outputs) = build_data_lists(file)
-	return (np.array(features, dtype=int), np.array(outputs, dtype=int))
+    (features, outputs) = build_data_lists(file)
+    return (np.array(features, dtype=int), np.array(outputs, dtype=int))
 
 def build_data_lists(file):
-	(features, outputs) = ([], [])
-	for line in file:
-		(line_features, line_output) = extract_features_and_output(line)
-		features.append(line_features)
-		outputs.append(line_output)
-	return (features, outputs)
+    (features, outputs) = ([], [])
+    for line in file:
+        (line_features, line_output) = extract_features_and_output(line)
+        features.append(line_features)
+        outputs.append(line_output)
+    return (features, outputs)
 
 def extract_features_and_output(line):
-	features_and_output = map(int, line.split(','))
-	return (features_and_output[0:-1], features_and_output[-1])
+    features_and_output = map(int, line.split(','))
+    return (features_and_output[0:-1], features_and_output[-1])
 
 ########## Gradient Descent ##########
 
 def batch_gradient_descent(features, outputs, learning_rate, collect_acc_flag):
-	weight = np.zeros_like(features[0], dtype=float)
-	#learning_rate = 0.01
-	epsilon = 0.0000001
-	iterations = 0
-	
-	if collect_acc_flag == True:
-		acc_file = open("data/acc_data.txt", 'w')
-		(testing_features, testing_outputs) = get_data_arrays(
-			"data/usps-4-9-test.csv")
-	
-	while True:
-		old_norm = norm_of_gradient(weight)
-		weight += learning_rate * update(features, outputs, weight)
-		new_norm = norm_of_gradient(weight)
-		iterations += 1
-		
-		if collect_acc_flag == True:
-			test_acc = test_weight(testing_features, testing_outputs, weight)
-			train_acc = test_weight(features, outputs, weight)
-			acc_file.write(str(train_acc) + "," + str(test_acc) + "\n")
-			
-		if abs(old_norm - new_norm) < epsilon:
-			print "Convered at Iteration: " + str(iterations)
-			return weight
+    weight = np.zeros_like(features[0], dtype=float)
+    #learning_rate = 0.01
+    epsilon = 0.0000001
+    iterations = 0
+    
+    if collect_acc_flag == True:
+        acc_file = open("data/acc_data.txt", 'w')
+        (testing_features, testing_outputs) = get_data_arrays(
+            "data/usps-4-9-test.csv")
+    
+    while True:
+        old_norm = norm_of_gradient(weight)
+        weight += learning_rate * update(features, outputs, weight)
+        new_norm = norm_of_gradient(weight)
+        iterations += 1
+        
+        if collect_acc_flag == True:
+            test_acc = test_weight(testing_features, testing_outputs, weight)
+            train_acc = test_weight(features, outputs, weight)
+            acc_file.write(str(train_acc) + "," + str(test_acc) + "\n")
+            
+        if abs(old_norm - new_norm) < epsilon:
+            print "Convered at Iteration: " + str(iterations)
+            return weight
   
-			
+            
 def norm_of_gradient(weight):
-	return norm(np.gradient(weight))			
-			
+    return norm(np.gradient(weight))            
+            
 def update(features, outputs, weight):
-	return (outputs - sigmoid(weight, features)).dot(features)
+    return (outputs - sigmoid(weight, features)).dot(features)
 
 def sigmoid(weight, features):
-	#prevent overflow...a low number would round to zero anyways
-	
-	prod = weight.dot(features.T)
-	arbitrarily_small_num = -99
-	for i, val in enumerate(prod):
-		if val <= arbitrarily_small_num:  
-			prod[i] = arbitrarily_small_num
-			
+    #prevent overflow...a low number would round to zero anyways
+    
+    prod = weight.dot(features.T)
+    arbitrarily_small_num = -99
+    for i, val in enumerate(prod):
+        if val <= arbitrarily_small_num:  
+            prod[i] = arbitrarily_small_num
+            
    
-	return 1 / (1 + np.exp(-prod))
+    return 1 / (1 + np.exp(-prod))
 
 def test_weight(features, outputs, weight):
-	guesses = sigmoid(weight, features)
-	right_tally = 0
-	wrong_tally = 0
-	for i, guess in enumerate(guesses):
-		if abs(guess-outputs[i]) <= 0.5:
-			right_tally += 1
-		else:
-			wrong_tally += 1
-	acc = float(right_tally) / (right_tally + wrong_tally)
-	return acc
-		
+    guesses = sigmoid(weight, features)
+    right_tally = 0
+    wrong_tally = 0
+    for i, guess in enumerate(guesses):
+        if abs(guess-outputs[i]) <= 0.5:
+            right_tally += 1
+        else:
+            wrong_tally += 1
+    acc = float(right_tally) / (right_tally + wrong_tally)
+    return acc
+    
+def loss_function(y):
+    pass
+  
+#Really cool function to show the weights
+def plot_weight(weight):
+    image = plt.imshow(np.reshape(weight, (16, 16)).T)
+    plt.show(image)
+    while 1:
+        pass
+  
 main()
