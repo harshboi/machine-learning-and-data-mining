a390f3475dd8acf027a19824837f7cc92917d64d
Jordan Crane <jordancrane@me.com>
2017-04-19 18:41:46 -0700

    Modularize problem functions

--- assignment-2/handwriting.py
+++ assignment-2/handwriting.py
@@ -21,17 +21,25 @@ labels = Labels(0, 1)
 ########## Main ##########
 
 def main():
-    (training_features, training_outputs) = get_data_arrays(
+    training_data = get_data_arrays(
             "data/usps-4-9-train.csv")
-    (testing_features, testing_outputs) = get_data_arrays(
+    testing_data = get_data_arrays(
             "data/usps-4-9-test.csv")
 
+    prob1(training_data, testing_data)
+
+    prob2(training_data)
+
+    prob3(training_data, testing_data)
+
+########## Problems ##########
+def prob1((training_features, training_outputs),
+          (testing_features, testing_outputs)):
     print "***** Problem 1 *****"
     for i in range(5):
-
         learning_rate = float(10)/(10**i)
         print "Learning Rate of: " + str(learning_rate)
-        weight = batch_gradient_descent(training_features, training_outputs, learning_rate, False)
+        weight = batch_gradient_descent(training_features, training_outputs, learning_rate)
 
         train_acc = test_weight(training_features, training_outputs, weight)
 
@@ -41,17 +49,20 @@ def main():
 
         print "Testing Data Accuracy: " + str(test_acc*100) + "%"
 
+def prob2((training_features, training_outputs)):
     print "***** Problem 2 *****"
     print "....Generating Accuracy Data..."
     final_weight = batch_gradient_descent(training_features, training_outputs, 0.01, True)
     print "Done"
 
+def prob3((training_features, training_outputs),
+          (testing_features, testing_outputs)):
     print "***** Problem 3 *****"
-
     for i in range(7):
         scalar = float(.001)*(10**i)
         print "Using a scaler of : " + str(scalar)
-        regularized_weight = regularization_gradient_descent(training_features, training_outputs, 0.01, scalar)
+        regularized_weight = batch_gradient_descent(training_features,
+                training_outputs, 0.01, regularization=scalar)
         train_acc = test_weight(training_features, training_outputs, regularized_weight)
         test_acc = test_weight(testing_features, testing_outputs, regularized_weight)
         print "Training Data Accuracy with Regularization: " + str(train_acc*100) + "%"
@@ -83,7 +94,8 @@ def extract_features_and_output(line):
 
 ########## Gradient Descent ##########
 
-def batch_gradient_descent(features, outputs, learning_rate, collect_acc_flag):
+def batch_gradient_descent(features, outputs, learning_rate,
+        collect_acc_flag=False, regularization=None):
     weight = np.zeros_like(features[0], dtype=float)
     #learning_rate = 0.01
     epsilon = 0.0000001
@@ -96,7 +108,8 @@ def batch_gradient_descent(features, outputs, learning_rate, collect_acc_flag):
 
     while True:
         old_norm = norm_of_gradient(weight)
-        weight += learning_rate * update(features, outputs, weight)
+        weight += learning_rate * update(features, outputs, weight,
+                regularization)
         new_norm = norm_of_gradient(weight)
         iterations += 1
 
@@ -112,14 +125,12 @@ def batch_gradient_descent(features, outputs, learning_rate, collect_acc_flag):
 def norm_of_gradient(weight):
     return norm(np.gradient(weight))
 
-def update(features, outputs, weight):
-    return (outputs - sigmoid(weight, features)).dot(features)
-
-def update_regular(features, outputs, weight, scalar):
-    loss_function = (outputs - sigmoid(weight, features)).dot(features)
-    for i, loss in enumerate(loss_function):
-        loss_function[i] += 0.5 * scalar * norm(weight)
-    return loss_function
+def update(features, outputs, weight, regularization):
+    if not regularization:
+        return (outputs - sigmoid(weight, features)).dot(features)
+    else:
+        return ((outputs - sigmoid(weight, features)).dot(features) +
+                0.5 * regularization * norm(weight))
 
 def sigmoid(weight, features):
     exponents = weight.dot(features.T)
@@ -143,21 +154,6 @@ def test_weight(features, outputs, weight):
             wrong_tally += 1
     acc = float(right_tally) / (right_tally + wrong_tally)
     return acc
-
-def regularization_gradient_descent(features, outputs, learning_rate, scalar):
-    weight = np.zeros_like(features[0], dtype=float)
-    epsilon = 0.0000001
-    iterations = 0
-
-    while True:
-        old_norm = norm_of_gradient(weight)
-        weight += learning_rate * update_regular(features, outputs, weight, scalar)
-        new_norm = norm_of_gradient(weight)
-        iterations += 1
-
-        if abs(old_norm - new_norm) < epsilon:
-            print "Convered at Iteration: " + str(iterations)
-            return weight
 
 #Really cool function to show the weights
 def plot_weight(weight):
