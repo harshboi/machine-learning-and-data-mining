34f4a4296eeb4ba9d758bff382a52aec5af2fcf6
Jordan Crane <jordancrane@me.com>
2017-05-11 23:57:34 -0700

    Flesh out k-means implementation

--- assignment-4/cluster.py
+++ assignment-4/cluster.py
@@ -12,6 +12,8 @@ import random
 from collections import namedtuple
 from operator import attrgetter
 
+Cluster = namedtuple('Cluster', 'center points')
+
 class Data:
     def __init__(self, filename=None):
         file = open(filename)
@@ -38,16 +40,45 @@ class Kmeans:
     def __init__(self, data, k=2):
         self.data = data
         self.k = k
-        self.centers = []
+        self.clusters = []
+        self._initialize_clusters()
 
-    def initialize_centers(self):
+    def cluster(self):
+        converged = False
+        while not converged:
+            for x in self.data:
+                self._assign(x)
+            self._update()
+
+    def _assign(self, x):
+        min_d = 999999
+        for cluster in self.clusters:
+            d = self._distance(cluster.center, x)
+            if d < min_d:
+                min_d = d
+                choice = cluster
+        choice.points.extend([x])
+
+    def _update(self):
+        for i, cluster in enumerate(self.clusters):
+            print len(cluster.points)
+            #FIXME All points are going to the first cluster for some reason
+            new_center = 1 / len(cluster.points) * np.sum(cluster.points)
+            new_cluster = Cluster(new_center, [])
+            del self.clusters[i]
+            self.clusters.append(new_cluster)
+
+    def _distance(self, a, b):
+        return np.sum(np.absolute(a - b))
+
+    def _initialize_clusters(self):
         indexes = np.random.choice(len(self.data), self.k, replace=False)
         for i in indexes:
-            self.centers.append(self.data[i])
+            self.clusters.append(Cluster(self.data[i], []))
 
 def main():
     data = Data("data/data.txt")
     k_means = Kmeans(data)
-    k_means.initialize_centers()
+    k_means.cluster()
 
 if __name__ == '__main__':main()
