922ff3d4748766ee0be581d38669ba4f6ba2e610
Jordan Crane <jordancrane@me.com>
2017-05-12 21:23:38 -0700

    Move Cluster into its own class

--- assignment-4/cluster.py
+++ assignment-4/cluster.py
@@ -7,12 +7,16 @@
 
 import math
 from math import log
+import sys
 import numpy as np
 import random
 from collections import namedtuple
 from operator import attrgetter
 
-Cluster = namedtuple('Cluster', 'center points')
+class Cluster:
+    def __init__(self, center, points=[]):
+        self.center = center
+        self.points = points
 
 class Data:
     def __init__(self, filename=None):
@@ -51,29 +55,29 @@ class Kmeans:
             self._update()
 
     def _assign(self, x):
-        min_d = 999999
+        min_d = sys.maxint
         for cluster in self.clusters:
+            print min_d
             d = self._distance(cluster.center, x)
             if d < min_d:
                 min_d = d
                 choice = cluster
-        choice.points.extend([x])
+        choice.points.append(x)
 
     def _update(self):
         for i, cluster in enumerate(self.clusters):
-            print len(cluster.points)
-            #FIXME All points are going to the first cluster for some reason
             new_center = 1 / len(cluster.points) * np.sum(cluster.points)
-            new_cluster = Cluster(new_center, [])
-            del self.clusters[i]
-            self.clusters.append(new_cluster)
+            cluster.center = new_center
 
     def _distance(self, a, b):
         return np.sum(np.absolute(a - b))
+        return np.sum(a*b)/(np.sum(np.square(a))*np.sum(np.square(b)))
 
     def _initialize_clusters(self):
         indexes = np.random.choice(len(self.data), self.k, replace=False)
+        print indexes
         for i in indexes:
+            print self.data[i]
             self.clusters.append(Cluster(self.data[i], []))
 
 def main():
