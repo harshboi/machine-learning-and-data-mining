6410067bb2b3fbfe1b1e56b81980ce83b7aa57de
brahmstadtn <brahmstadtn@gmail.com>
2017-06-04 20:02:04 -0700

    Overall, getting 64% accuracy. Data being missed is 50/50, so now we need to add features, or work this with another classification method.

--- final_project/question_pairs.py
+++ final_project/question_pairs.py
@@ -12,7 +12,6 @@ from collections import namedtuple
 from operator import attrgetter
 import statistics as stats
 
-
 class QuestionPair:
     def __init__(self, string):
         string = string.split(',')
@@ -28,7 +27,8 @@ class QuestionPair:
 def main():
     train_file = open('data/sentences.csv')
     word_file = open('words.txt', 'w')  
-    wrongs_file = open('wrongs.txt', 'w')
+    wrongs_file = open('wrongs.csv', 'w')
+    features_file = open('features.csv', 'w')
     word_dict = dict()
     print "Counting"
     total_words = 0
@@ -94,51 +94,47 @@ def main():
     negatives = []
     
     for score in scores:     
-        if score[1] == 1:
-            pos_count += 1
-            pos_average += score[0]
+        if score[1] == 1:   
             positives.append(score[0])
-        else:
-            neg_count += 1
-            neg_average += score[0]
+            guess = 1
+        else:   
             negatives.append(score[0])
-           
-    pos_average = pos_average / pos_count   
-    neg_average = neg_average / neg_count
-    print "Difference score for positives:"
-    print pos_average
-    print "Difference score for negatives:"
-    print neg_average
-    print "STD"
-    print stats.stdev(positives)
-    pos_point = stats.median(positives)
-
-    print stats.stdev(negatives)
-    neg_point = stats.median(negatives)
-    decision_point = neg_average - pos_average
-    
+            guess = -1
+        
+   
     right = 0
     wrong = 0
+    false_positives = 0
     abstains = 0
-    
-    
+    neg_point = stats.mean(negatives)
+    pos_point = stats.mean(positives)
+ 
     for score in scores:
         if score[0] > neg_point:
             predict = 0
         elif score[0] < pos_point:
             predict = 1
         else:
-            predict = None
+            predict = 0
             abstains += 1
+          
         if not predict == None:
             if predict == score[1]:
                 right += 1
             else:
                 wrong += 1
+                if predict == 1:
+                    false_positives += 1
                 wrongs_file.write(str(score) + '\n')
                 
+  
+    print "Decision Point:"
+    print pos_point          
     print "Accuracy of what I said:"        
     print float(right)/(right+wrong)
     print "Percent abstained:"
     print float(abstains)/(abstains+right+wrong)
+    print "False positive percentage of wrongs:"
+    print float(false_positives)/wrong
+    
 main()   
