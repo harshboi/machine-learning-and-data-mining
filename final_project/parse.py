dda5e69b3000e85aedf60c6c14b7a4fedf908324
Jordan Crane <jordancrane@me.com>
2017-06-09 14:49:59 -0700

    Fix punctuation removal

--- final_project/parse.py
+++ final_project/parse.py
@@ -7,22 +7,12 @@ import os
 import re
 import csv
 import codecs
-#import itertools
 import numpy as np
-#import pandas as pd
 
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from string import punctuation
-
-#import keras
-#from keras.preprocessing.text import Tokenizer
-#from keras.preprocessing.sequence import pad_sequences
-#from keras.layers import Input, LSTM, Dense
-#from keras.models import Model
-#from sklearn.feature_extraction.text import CountVectorizer
-#from sklearn.model_selection import test train split
 
 TRAIN_DATA_FILE = 'data/train.csv'
 OUTPUT_TRAIN_DATA_FILE = 'outputs/parsed_questions.csv'
@@ -33,15 +23,18 @@ OUTPUT_TEST_DATA_FILE = 'outputs/parsed_test_data.csv'
 ########################################
 ## process texts in datasets
 ########################################
-print('Processing text dataset')
 
 # The function "text_to_wordlist" is modified from
 # https://www.kaggle.com/currie32/quora-question-pairs/the-importance-of-cleaning-text
 def text_to_wordlist(text, remove_stopwords=False, remove_punctuation=False, stem_words=False):
     # Clean the text, with the option to remove stopwords and to stem words.
 
-    # Convert words to lower case and split them
-    text = text.lower().split()
+    # Convert words to lower case
+    text = text.lower()
+
+    # Optionally, remove punctuation
+    if remove_punctuation:
+        text = text.translate(str.maketrans('','',punctuation))
 
     # Optionally, remove stop words
     if remove_stopwords:
@@ -50,16 +43,13 @@ def text_to_wordlist(text, remove_stopwords=False, remove_punctuation=False, ste
         except:
             nltk.download('stopwords')
             stops = set(stopwords.words("english"))
+        text = text.split()
         text = [w for w in text if not w in stops]
-
-    # Optionally, remove punctuation
-    if remove_punctuation:
-        text = [w for w in text if not w in punctuation]
-
-    text = " ".join(text)
+        text = " ".join(text)
 
     # Clean the text
     #text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+    text = re.sub(r"[()]", "", text)
     text = re.sub(r"what's", "what is ", text)
     text = re.sub(r"\'s", " ", text)
     text = re.sub(r"\'ve", " have ", text)
@@ -106,57 +96,41 @@ def text_to_wordlist(text, remove_stopwords=False, remove_punctuation=False, ste
     # Return a list of words
     return(text)
 
-#Import data
-#train = pd.read_csv("data/train.csv")
+print('Processing training dataset')
+i = 0
+with codecs.open(OUTPUT_TRAIN_DATA_FILE,'w','utf-8') as output_train_file:
+    with codecs.open(TRAIN_DATA_FILE, encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=',')
+        header = next(reader)
+        for values in reader:
+            i += 1
+            output_train_file.write(
+                "".join([
+                    text_to_wordlist(values[3], remove_stopwords=True,
+                        remove_punctuation=True), ",",
+                    text_to_wordlist(values[4], remove_stopwords=True,
+                        remove_punctuation=True), ",",
+                    values[5], "\n"
+                ])
+            )
 
-texts_1 = []
-texts_2 = []
-label_list = []
+print('Found ', i, ' texts in train.csv')
 
-output_train_file = codecs.open(OUTPUT_TRAIN_DATA_FILE,'w+','utf-8')
-output_test_file = codecs.open(OUTPUT_TEST_DATA_FILE,'w','utf-8')
+print('Processing testing dataset')
+i = 0
+with codecs.open(OUTPUT_TEST_DATA_FILE,'w','utf-8') as output_test_file:
+    with codecs.open(TEST_DATA_FILE, encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=',')
+        header = next(reader)
+        for values in reader:
+            i += 1
+            output_test_file.write(
+                "".join([
+                    text_to_wordlist(values[1], remove_stopwords=True,
+                        remove_punctuation=True),",",
+                    text_to_wordlist(values[2], remove_stopwords=True,
+                        remove_punctuation=True), "\n"
+                ])
+            )
 
-with codecs.open(TRAIN_DATA_FILE, encoding='utf-8') as f:
-    reader = csv.reader(f, delimiter=',')
-    header = next(reader)
-    for values in reader:
-        texts_1.append(text_to_wordlist(values[3], remove_stopwords=True,
-            remove_punctuation=True))
-        texts_2.append(text_to_wordlist(values[4], remove_stopwords=True,
-            remove_punctuation=True))
-        label_list.append(int(values[5]))
-        output_train_file.write(texts_1[-1] + "," + texts_2[-1] + "," + str(label_list[-1]) + ",\n")
-        
-with codecs.open(TEST_DATA_FILE, encoding='utf-8') as f:
-    reader = csv.reader(f, delimiter=',')
-    header = next(reader)
-    for values in reader:
-        texts_1.append(text_to_wordlist(values[1], remove_stopwords=True,
-            remove_punctuation=True))
-        texts_2.append(text_to_wordlist(values[2], remove_stopwords=True,
-            remove_punctuation=True))
-
-        output_test_file.write(texts_1[-1] + "," + texts_2[-1] + ",\n")
-#questions_1 = np.array(texts_1)
-#questions_2 = np.array(texts_2)
-#labels = np.array(label_list)
-print('Found %s texts in train.csv' % len(texts_1))
-
-#Sanitize data
-#for i in range(10):
-#    texts_1.append(text_to_wordlist(train.question1[i]))
-#    texts_2.append(text_to_wordlist(train.question2[i]))
-#    label_list.append(int(train.is_duplicate[i]))
-#
-#questions_1 = np.array(texts_1)
-#questions_2 = np.array(texts_2)
-#labels = np.array(label_list)
-#print('Found %s texts in train.csv' % len(texts_1))
-#
-##Create Vocab
-#count_vectorizer = CountVectorizer(max_features=10000-1).fit(
-#        itertools.chain(questions_1, questions_2))
-#other_index = len(count_vectorizer.vocabulary_)
-
-#Prep data
-
+print('Found ', i, ' texts in test.csv')
